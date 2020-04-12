de735902add278db7d298c4f00dd16ab2266058a
Tom Rushton <tom.rushton@hotmail.co.uk>
2020-04-12 14:56:10 +0100

    Refactor unnecessary intermediate event handlers

--- Chromatic/UI/UnitPortrait/UnitPortrait.gd
+++ Chromatic/UI/UnitPortrait/UnitPortrait.gd
@@ -1,8 +1,25 @@
 extends Control
 
+#Fields
 onready var icon_node = get_node("Panel/HBoxContainer/Border/Icon")
 onready var name_node = get_node("Panel/HBoxContainer/Name")
 
+
+#Event Handlers
+func _on_Map_unit_selected(unit: Unit) -> void:
+	if !unit:
+		print_debug("Unit selected: No unit!")
+		return
+	
+	set_unit(unit)
+	visible = true
+
+
+func _on_Map_unit_deselected() -> void:
+	visible = false
+
+
+#Methods
 func set_unit(unit: Unit) -> void:
 	icon_node.texture = unit.get_node("Sprite").texture
 	name_node.text = unit.name
